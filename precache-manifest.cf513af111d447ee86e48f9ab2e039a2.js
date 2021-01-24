e9d77d77294dd9b91ce3e19029dedf175dc175f7
Cody Ley-Han <11651981+codyleyhan@users.noreply.github.com>
2021-01-24 01:53:18 -0800

    deploy

---
+++ precache-manifest.cf513af111d447ee86e48f9ab2e039a2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1387667ca5b0ee9cd50b3b704fa57b5",
+    "url": "/android-chrome-144x144.png"
+  },
+  {
+    "revision": "574b34d7d2b03bbd3416ff2bded74df2",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "2e842492f07d62e433f1",
+    "url": "/css/app.2886d062.css"
+  },
+  {
+    "revision": "267d4ddcbc0891ebc68bd3959260a5a7",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "c35b0d69c56cb4ca4fd224de7f5d617f",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "651a88fc1dcf77a3e030a949da88cebb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e842492f07d62e433f1",
+    "url": "/js/app.a9262f21.js"
+  },
+  {
+    "revision": "a00d149689edd3086e0e",
+    "url": "/js/chunk-vendors.4fc043a5.js"
+  },
+  {
+    "revision": "7118fac69a4145c497c628123d33090f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "8c9d24db10ce1f7fc8ff1c52bc25584e",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3f6e5c5e067df96a8a4f300d9b443180",
+    "url": "/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "108b826a6e1dabd16cddde2129ae13bc",
+    "url": "/site.webmanifest"
+  }
+]);
